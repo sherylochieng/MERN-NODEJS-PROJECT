cda6734d7e7093a584a0841dff9ecfe27be83b97
sherylOchieng <sadhiambo998@gmail.com>
2026-03-17 20:19:05 +0300

    add CHALK DEPENDECY EXAMPLES and CRYPTORANDOMSTRING DEPENDECY EXAMPLES

--- node-Backend/index.js
+++ node-Backend/index.js
@@ -1,5 +1,8 @@
 import cryptoRandomString from 'crypto-random-string';
+import chalk from 'chalk';
 
+
+console.log('CRYPTORANDOMSTRING DEPENDECY EXAMPLES:');
 const userCode = 'rsrr';
 const correctCode='rsrr';
 
@@ -12,3 +15,42 @@ if (userCode==correctCode){
 else{
     console.log('user entered the wrong code');
 }
+
+//CHALK
+console.log('CHALK DEPENDECY EXAMPLES:');
+console.log(chalk.blue('This is a blue text'));
+console.log(chalk.red('This is a red text'));
+console.log(chalk.green('This is a green text'));
+
+
+const log = console.log;
+
+// Combine styled and normal strings
+log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+
+// Compose multiple styles using the chainable API
+log(chalk.blue.bgRed.bold('Hello world!'));
+
+// Pass in multiple arguments
+log(chalk.blue('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz'));
+
+// Nest styles
+log(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'));
+
+// Nest styles of the same type even (color, underline, background)
+log(chalk.green(
+	'I am a green line ' +
+	chalk.blue.underline.bold('with a blue substring') +
+	' that becomes green again!'
+));
+
+
+const error = chalk.bold.red;
+const warning = chalk.hex('#FFA500'); // Orange color
+
+console.log(error('Error!'));
+console.log(warning('Warning!'));
+
+const name = 'Sindre';
+console.log(chalk.green('Hello %s'), name);
+//=> 'Hello Sindre
